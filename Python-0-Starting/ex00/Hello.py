2d0326aac3cb81fecd6e9f4d1fc9de1cacffbaae
Youness Amzil <amzilyouness@gmail.com>
2026-09-16 16:33:07 +0100

    done with ex00

---
+++ Python-0-Starting/ex00/Hello.py
@@ -0,0 +1,15 @@
+ft_list = ["Hello"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "Hello", "tutu!"}
+ft_dict = {"Hello" : "titi!"}
+
+ft_list.append("World!")
+ft_tuple = (ft_tuple[0], "Morocco!")
+ft_set.remove("tutu!")
+ft_set.add("Tetouan!")
+ft_dict["Hello"] = "1337Med!"
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
